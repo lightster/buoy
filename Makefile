952ef717f9ef28470f96f47c536cd7f00e520833
Matt Light <matt.light@lightdatasys.com>
2017-12-27 22:38:14 -0800

    Add https support to buoy

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
 install:
 	(docker network inspect pier-buoy || docker network create pier-buoy) 1>/dev/null 2>/dev/null
 	docker-compose build
+	$(MAKE) data/ssl/certs/dev.crt
 	docker-compose up -d
+
+data/ssl/certs/dev.crt:
+	docker-compose run --rm dev-ssl generate
---
+++ docker/dev-ssl/docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+COMMAND="$1"
+
+case "${COMMAND}" in
+
+    generate)
+        if [ -f /etc/ssl/certs/dev.crt ]; then
+            exit 0
+        fi
+
+        mkdir -p /etc/ssl/certs
+        cd /etc/ssl/certs
+
+        openssl genrsa -out "dev.key" 4096
+
+        openssl req -new -key "dev.key" -out "dev.csr" -sha256 \
+            -subj '/C=US/ST=CA/L=Irvine/O=Developer/CN=*.p.com'
+
+        openssl x509 -req -days 750 -in "dev.csr" -signkey "dev.key" -sha256 \
+            -out "dev.crt" -extensions server
+        ;;
+
+    sh)
+        /bin/sh
+        ;;
+
+    *)
+        echo "Usage: {generate|sh}"
+        exit 1
+
+esac
