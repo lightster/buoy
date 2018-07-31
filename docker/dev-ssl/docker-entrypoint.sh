e31323f395e06fd55ebedcfdaff8e366b0a81164
Matt Light <matt@paren.io>
2018-07-30 22:45:57 -0700

    Set Subject Alternative Names when generating cert
    Chrome requires SANs even when the certificate is manually imported
    to Keychain and marked as trusted

--- docker/dev-ssl/docker-entrypoint.sh
+++ docker/dev-ssl/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 COMMAND="$1"
 DOMAIN="${DOMAIN:-b.com}"
@@ -13,13 +13,37 @@ case "${COMMAND}" in
         mkdir -p /etc/ssl/certs
         cd /etc/ssl/certs
 
-        openssl genrsa -out "dev.key" 4096
+        openssl req -nodes -new -newkey rsa:2048 -keyout "dev.key" -out "dev.csr" -sha256 -config <(
+cat <<-EOF
+[req]
+default_bits = 2048
+prompt = no
+default_md = sha256
+distinguished_name = dn
 
-        openssl req -new -key "dev.key" -out "dev.csr" -sha256 \
-            -subj "/C=US/ST=CA/L=Irvine/O=Developer/CN=*.${DOMAIN}"
+[ dn ]
+C=US
+ST=CA
+L=Irvine
+O=Developer
+OU=Dev Domain
+emailAddress=admin@${DOMAIN}
+CN=*.${DOMAIN}
+EOF
+)
 
-        openssl x509 -req -days 750 -in "dev.csr" -signkey "dev.key" -sha256 \
-            -out "dev.crt" -extensions server
+        openssl x509 -req -days 750 -in "dev.csr" -signkey "dev.key" -sha256 -out "dev.crt" -extfile <(
+cat <<-EOF
+authorityKeyIdentifier=keyid,issuer
+basicConstraints=CA:FALSE
+keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+subjectAltName = @alt_names
+
+[alt_names]
+DNS.1 = ${DOMAIN}
+DNS.2 = *.${DOMAIN}
+EOF
+                )
         ;;
 
     sh)
